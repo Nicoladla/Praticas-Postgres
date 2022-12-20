0692ad10fc240f758fd3735c3d70fce8acc55d69
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-20 18:46:45 -0300

    feat: prática de modelagem 2

--- Prática - Modelagem (integridade)/6.sql
+++ Prática - Modelagem (integridade)/6.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "customers"(
     id SERIAL PRIMARY KEY,
     "fullName" TEXT NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     password TEXT NOT NULL
 )
