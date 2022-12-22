2904c547e09adc6d7f8875f2dbc330d8bbdee556
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-12-21 22:22:01 -0300

    style: prática do curso com imagem

--- Prática: Modelagem de Banco (Curso)/6.sql
+++ Prática: Modelagem de Banco (Curso)/6.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "projects" (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
-    module_id INTEGER NOT NULL REFERENCES module(id)
+    module_id INTEGER NOT NULL REFERENCES modules(id)
 );
